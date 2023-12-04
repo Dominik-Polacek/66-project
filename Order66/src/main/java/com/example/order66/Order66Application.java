ef7d5c1b0bddb5624894349d4bba733d3896d46c
Matous <matous.kovarovic@seznam.cz>
2023-12-04 12:58:13 +0100

    first commit

---
+++ Order66/src/main/java/com/example/order66/Order66Application.java
@@ -0,0 +1,13 @@
+package com.example.order66;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Order66Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Order66Application.class, args);
+    }
+
+}
